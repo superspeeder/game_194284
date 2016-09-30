4d8c43b7ba38f30ae97c069809302fddf27dc683
superspeeder <andy@delusion.org>
2016-09-30 15:17:25 -0700

    Update Gravity.py

--- Tools/Gravity.py
+++ Tools/Gravity.py
@@ -1,2 +1,16 @@
 from BaseForces import SimpleDirectionalForce
 from ForceDecorators import *
+from ForceConstant import G_FORCE_UNIT_RATE, DIRECTION_DOWN
+from ForceFunctions import increase
+
+
+class GravityForce(SimpleDirectionalForce):
+  def __init__(self, sgs):
+    super(GravityForce, self).__init__(self, DIRECTION_DOWN)
+    self.gs = sgs
+    self.incg = G_FORCE_UNIT_RATE
+  
+  @CollisionDetect.down
+  def isOnGround(self):
+    x = bool()
+    return x
